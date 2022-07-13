36747694c1eb3b28c874003bf42666cb144be2c0
AndreyKapus <kapustnikov@ukr.net>
2022-07-13 20:48:07 +0300

    second task

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,18 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+const element = ingredients.map((option) => {
+  const listIngredientsRef = document.createElement("li");
+
+  listIngredientsRef.classList.add("item");
+  listIngredientsRef.textContent = option;
+
+  const listElementRef = document.querySelector("ul");
+  listElementRef.appendChild(listIngredientsRef);
+});
