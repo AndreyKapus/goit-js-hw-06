70e11c9dce48b29ea90c1cee8b00b6f56d9f5eff
AndreyKapus <kapustnikov@ukr.net>
2022-07-18 14:58:59 +0300

    task six

---
+++ js/task-06.js
@@ -0,0 +1,16 @@
+const inputRef = document.querySelector("input");
+const inputDataLength = inputRef.getAttribute("data-length");
+const inputValid = document.getElementById("validation-input").onblur;
+console.log(inputDataLength);
+console.log(inputValid);
+
+document.getElementById("validation-input").onblur = function () {
+  console.log(this.value.length);
+  if (this.getAttribute("data-length") > this.value.length) {
+    this.classList.remove("valid");
+    this.classList.add("invalid");
+  } else {
+    this.classList.remove("invalid");
+    this.classList.add("valid");
+  }
+};
