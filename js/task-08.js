e07cdbf7427967853239dade71c175059b3ee898
AndreyKapus <kapustnikov@ukr.net>
2022-07-19 16:05:39 +0300

    task 8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,41 @@
+const formRef = document.querySelector(".login-form");
+console.log(formRef);
 
+formRef.addEventListener("submit", onFormSubmit);
+
+function onFormSubmit(event) {
+  event.preventDefault();
+  const formElements = event.currentTarget.elements;
+
+  const mail = formElements.email.value;
+  const password = formElements.password.value;
+
+  if (mail === "" || password === "") {
+    alert("Нужно заполнить все поля!");
+  }
+  const dataArray = {
+    mail,
+    password,
+  };
+  console.log(dataArray);
+}
+
+// const formRef = document.querySelector(".login-form");
+// console.log(formRef);
+
+// formRef.addEventListener("submit", onFormSubmit);
+
+// function onFormSubmit(event) {
+//   event.preventDefault();
+//   const formData = new FormData(event.currentTarget);
+//   formData.forEach((value, name) => {
+//     if (value === "") {
+//       alert("Нужно заполнить все поля!");
+//     }
+//     const dataArray = {
+//       name,
+//       value,
+//     };
+//     console.log(dataArray);
+//   });
+// }
