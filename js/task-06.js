5f9250753f3253621bd7216be55fcbdcaa74720d
AndreyKapus <kapustnikov@ukr.net>
2022-07-20 19:58:59 +0300

    task 6 remastered

--- js/task-06.js
+++ js/task-06.js
@@ -6,8 +6,8 @@ console.log(inputValid);
 
 document.getElementById("validation-input").onblur = function () {
   console.log(this.value.length);
-  const dataLength = this.getAttribute("data-length");
-  if (dataLength > this.value.length || dataLength < this.value.length) {
+  const dataLength = Number(this.getAttribute("data-length"));
+  if (dataLength !== this.value.length) {
     this.classList.remove("valid");
     this.classList.add("invalid");
   } else {
