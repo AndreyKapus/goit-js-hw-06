847223193401da754bd33150af13ba377284c803
AndreyKapus <kapustnikov@ukr.net>
2022-07-20 13:26:56 +0300

    task 9

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,14 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const body = document.querySelector("body");
+const button = document.querySelector(".change-color");
+const spanColor = document.querySelector(".color");
+
+button.addEventListener("click", () => {
+  body.style.backgroundColor = getRandomHexColor();
+  const bodyColorValue = document.body.style.backgroundColor;
+  spanColor.textContent = bodyColorValue;
+  console.log(bodyColorValue);
+});
